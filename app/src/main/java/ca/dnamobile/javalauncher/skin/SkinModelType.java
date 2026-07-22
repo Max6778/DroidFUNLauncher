4a945116c12f85a33b28266e1ce3ce980c00eb33
Epic <tractionbruh@gmail.com>
2026-07-22 11:05:07 +0100

    Add SkinModelType.java

---
+++ app/src/main/java/ca/dnamobile/javalauncher/skin/SkinModelType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 DNA Mobile Applications.
+ * DroidBridge / FlintLauncher fork addition.
+ */
+
+package ca.dnamobile.javalauncher.skin;
+
+/**
+ * The two Minecraft skin arm models.
+ * CLASSIC = "Steve" (4px wide arms), SLIM = "Alex" (3px wide arms).
+ */
+public enum SkinModelType {
+    CLASSIC,
+    SLIM
+}
